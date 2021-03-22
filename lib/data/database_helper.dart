16054fb349a66bbad72d8499479fb01a61981104
Ramiro Trujillo <trujasra@gmail.com>
2021-03-22 18:45:32 -0400

    se adiciono datos a la coneccion de BD

--- lib/data/database_helper.dart
+++ lib/data/database_helper.dart
@@ -13,4 +13,74 @@ class DataBaseHelper {
   static const String LAST_NAME = 'apellidos';
   static const String TABLE = 'Persona';
   static const String DB_NAME = 'db_persona.db';
+
+  Future<Database> get dbase async {
+    // Verifica si la BD es diferente de nulo
+    if (_db != null) {
+      return _db;
+    }
+
+    // envia al metodo para inicializar la BD
+    _db = await iniciaDB();
+    return _db;
+  }
+
+  iniciaDB() async {
+    io.Directory documentoDirectorio = await getApplicationDocumentsDirectory();
+    String path = join(documentoDirectorio.path, DB_NAME);
+    var db = await openDatabase(path, version: 1, onCreate: _onCreate);
+    return db;
+  }
+
+  FutureOr<void> _onCreate(Database db, int version) async {
+    String sql =
+        "CREATE TABLE $TABLE ($ID INTEGER PRIMARY KEY, $NAME TEXT, $LAST_NAME TEXT)";
+    await db.execute(sql);
+  }
+
+  // Future<Persona> registraPersona(Persona persona) async {
+  //   var dbCliente = await dbase;
+  //   persona.id = await dbCliente.insert(TABLE, persona.toMap());
+  //   return persona;
+
+  //   /*await dbClient.transaction((txn) {
+  //     var query = "INSERT INTO $TABLE($NAME, $LAST_NAME) VALUES('" +
+  //         persona.nombre +
+  //         "','" +
+  //         persona.apellidos +
+  //         "')";
+  //     return txn.rawQuery(query);
+  //   });*/
+  // }
+
+  // Future<List<Persona>> obtienePersona() async {
+  //   var dbClient = await dbase;
+  //   List<Map> maps =
+  //       await dbClient.query(TABLE, columns: [ID, NAME, LAST_NAME]);
+  //   // List<Map> maps = await dbClient.rawQuery("SELECT * FROM $TABLE");
+  //   List<Persona> listaPersona = [];
+  //   if (maps.length > 0) {
+  //     for (int i = 0; i < maps.length; i++) {
+  //       listaPersona.add(Persona.fromMap(maps[i]));
+  //     }
+  //   }
+  //   return listaPersona;
+  // }
+
+  // Future<int> eliminaPersona(int id) async {
+  //   var dbCliente = await dbase;
+  //   return await dbCliente.delete(TABLE, where: '$ID = ?', whereArgs: [id]);
+  // }
+
+  // Future<int> actualizaPersona(Persona persona) async {
+  //   var dbCliente = await dbase;
+  //   return await dbCliente.update(TABLE, persona.toMap(),
+  //       where: '$ID = ?', whereArgs: [persona.id]);
+  // }
+
+  // Future close() async {
+  //   var dbCliente = await dbase;
+  //   dbCliente.close();
+  // }
+
 }
