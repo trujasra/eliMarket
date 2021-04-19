bcca0cda605e58a94794e6f85e0eb0b4754fc194
RamiroTrujillo Almanza <ramiro.trujillo@oep.net>
2021-04-19 12:20:05 -0400

    Se realizo validaciones al formualrio de categoria

--- lib/pantallas/registro_categoria.dart
+++ lib/pantallas/registro_categoria.dart
@@ -16,7 +16,18 @@ class RegistroCategoriaPage extends StatefulWidget {
 }
 
 class _RegistroCategoriaPageState extends State<RegistroCategoriaPage> {
+  // LLaves par alos formularios
+  final _formKeyTipoCategoria = GlobalKey<FormState>();
+  final _formKeyCategoria = GlobalKey<FormState>();
+
+  // Estilos
   TextStyle textStyle = TextStyle(color: kTextoDarkColor);
+  OutlineInputBorder inputBorderTexto = OutlineInputBorder(
+      borderSide: new BorderSide(
+        color: kTextoColor,
+      ),
+      borderRadius: BorderRadius.circular(6.0));
+
   TextEditingController descripcionController = TextEditingController();
   TextEditingController observacionController = TextEditingController();
   TextEditingController descTipoCategoriaController = TextEditingController();
@@ -83,96 +94,102 @@ class _RegistroCategoriaPageState extends State<RegistroCategoriaPage> {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-                child: TextField(
-                  maxLength: 40,
-                  controller: descripcionController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: new BorderSide(
-                          color: kTextoColor,
-                        ),
-                        borderRadius: BorderRadius.circular(6.0)),
-                    labelText: 'Descripción',
-                    hintText: 'Ingrese la categoria',
-                    labelStyle: textStyle,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: new BorderSide(
-                          color: kPrimaryColor,
-                        ),
-                        borderRadius: BorderRadius.circular(6.0)),
+          Form(
+            key: _formKeyCategoria,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
+                  child: TextFormField(
+                    validator: (valor) {
+                      if (valor.isEmpty) {
+                        return 'Ingrese la categoria';
+                      }
+                      return null;
+                    },
+                    maxLength: 40,
+                    controller: descripcionController,
+                    decoration: InputDecoration(
+                      labelText: 'Descripción categoria',
+                      // hintText: 'Ingrese la categoria',
+                      labelStyle: textStyle,
+                      enabledBorder: inputBorderTexto,
+                      focusedBorder: inputBorderTexto,
+                      errorBorder: inputBorderTexto,
+                      focusedErrorBorder: inputBorderTexto,
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-                      child: Container(
-                        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-                        decoration: BoxDecoration(
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(6.0)),
-                            border: Border.all(
-                              color: kTextoColor,
-                              width: 1,
-                            ),
-                            color: Colors.white),
-                        child: DropdownButton(
-                          isExpanded: true,
-                          itemHeight: 50.0,
-                          style: textStyle,
-                          hint: Text(
-                            "Tipo categoria",
-                            style: TextStyle(
-                                color: kTextoDarkColor, fontSize: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                            left: 20.0, right: 2.0, top: 7.0, bottom: 7.0),
+                        child: DropdownButtonFormField(
+                          validator: (valor) {
+                            if (valor == null) {
+                              return "Seleccione tipo categoria";
+                            }
+                            return null;
+                          },
+                          decoration: new InputDecoration(
+                            labelText: "Tipo categoria",
+                            // hintText: 'Ingrese tipo categoria',
+                            labelStyle: textStyle,
+                            enabledBorder: inputBorderTexto,
+                            focusedBorder: inputBorderTexto,
+                            errorBorder: inputBorderTexto,
+                            focusedErrorBorder: inputBorderTexto,
                           ),
+                          isExpanded: true,
+                          // itemHeight: 50.0,
+                          style: textStyle,
                           value: vIdTipoCategoria,
+                          items: lista,
                           onChanged: (valor) {
+                            FocusScope.of(context)
+                                .requestFocus(new FocusNode());
                             // print(valor);
                             setState(() {
                               vIdTipoCategoria = valor;
                             });
                           },
-                          items: lista,
                         ),
                       ),
                     ),
-                  ),
-                  registrarNuevoTipoCategoria(context),
-                ],
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
-                child: TextField(
-                  maxLines: 2,
-                  controller: observacionController,
-                  decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                        borderSide: new BorderSide(
-                          color: kTextoColor,
-                        ),
-                        borderRadius: BorderRadius.circular(6.0)),
-                    labelText: 'Observación',
-                    hintText: 'Ingrese la observación',
-                    labelStyle: textStyle,
-                    focusedBorder: OutlineInputBorder(
-                        borderSide: new BorderSide(
-                          color: kTextoColor,
-                        ),
-                        borderRadius: BorderRadius.circular(6.0)),
+                    registrarNuevoTipoCategoria(context),
+                  ],
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.0, vertical: 7.0),
+                  child: TextFormField(
+                    validator: (valor) {
+                      if (valor.isEmpty) {
+                        return "Ingrese observación";
+                      }
+                      return null;
+                    },
+                    maxLines: 2,
+                    controller: observacionController,
+                    decoration: InputDecoration(
+                      labelText: 'Observación',
+                      // hintText: 'Ingrese la observación',
+                      labelStyle: textStyle,
+                      enabledBorder: inputBorderTexto,
+                      focusedBorder: inputBorderTexto,
+                      errorBorder: inputBorderTexto,
+                      focusedErrorBorder: inputBorderTexto,
+                    ),
                   ),
                 ),
-              ),
-              registrarNuevaCategoria(context)
-            ],
+                registrarNuevaCategoria(context)
+              ],
+            ),
           ),
         ],
       ),
@@ -193,54 +210,58 @@ class _RegistroCategoriaPageState extends State<RegistroCategoriaPage> {
       width: MediaQuery.of(context).size.width * 0.90,
       child: ElevatedButton(
         onPressed: () {
-          showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("Información"),
-                  content: Text("Desea registrar la nueva categoria?"),
-                  actions: [
-                    OutlinedButton(
-                      onPressed: () {
-                        // Categoria categoria = new Categoria();
-                        // categoria.descCategoria = descripcionController.text;
-                        // categoria.parTipoCategoria = int.parse(vIdTipoCategoria);
-                        // categoria.observacion = observacionController.text;
-                        // categoria.imagen = "assets/imagenes/abarrotes.png";
+          // verifica si la informacion esta validada.
+          if (_formKeyCategoria.currentState.validate()) {
+            showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text("Información"),
+                    content: Text("Desea registrar la nueva categoria?"),
+                    actions: [
+                      OutlinedButton(
+                        onPressed: () {
+                          // Categoria categoria = new Categoria();
+                          // categoria.descCategoria = descripcionController.text;
+                          // categoria.parTipoCategoria = int.parse(vIdTipoCategoria);
+                          // categoria.observacion = observacionController.text;
+                          // categoria.imagen = "assets/imagenes/abarrotes.png";
 
-                        // // Envia para registrar la informacion.
-                        // DataBaseHelper.db.registraCategoria(categoria);
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (BuildContext context) => MenuPage()));
-                      },
-                      child: Text(
-                        'SI',
-                        style: TextStyle(
-                          color: Colors.green,
+                          // // Envia para registrar la informacion.
+                          // DataBaseHelper.db.registraCategoria(categoria);
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (BuildContext context) =>
+                                      MenuPage()));
+                        },
+                        child: Text(
+                          'SI',
+                          style: TextStyle(
+                            color: Colors.green,
+                          ),
                         ),
+                        style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Colors.green)),
                       ),
-                      style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.green)),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      child: Text(
-                        'CANCELAR',
-                        style: TextStyle(
-                          color: Colors.red,
+                      OutlinedButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text(
+                          'CANCELAR',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
                         ),
+                        style: OutlinedButton.styleFrom(
+                            side: BorderSide(color: Colors.redAccent)),
                       ),
-                      style: OutlinedButton.styleFrom(
-                          side: BorderSide(color: Colors.redAccent)),
-                    ),
-                  ],
-                );
-              });
+                    ],
+                  );
+                });
+          }
         },
         child: Text("REGISTRAR"),
         style: ElevatedButton.styleFrom(
@@ -256,7 +277,7 @@ class _RegistroCategoriaPageState extends State<RegistroCategoriaPage> {
 
   Widget registrarNuevoTipoCategoria(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.only(right: 20.0),
+      padding: EdgeInsets.only(right: 20.0, left: 2.0),
       child: ElevatedButton.icon(
         icon: SvgPicture.asset(
           "assets/icons/shopping-basket.svg",
@@ -270,49 +291,72 @@ class _RegistroCategoriaPageState extends State<RegistroCategoriaPage> {
                 return Dialog(
                   shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15.0)),
-                  child: Container(
-                    height: 300.0,
-                    child: Column(
-                      children: [
-                        Container(
-                          padding: EdgeInsets.all(18.0),
-                          height: 60.0,
-                          width: MediaQuery.of(context).size.width,
-                          decoration: BoxDecoration(
-                              color: kPrimaryColor,
-                              borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(15.0),
-                                  topRight: Radius.circular(15.0))),
-                          child: Text(
-                            "Registrar Tipo de Categoria",
-                            style: GoogleFonts.berkshireSwash(
-                                color: kTextoLigthColor, fontSize: 18.0),
-                          ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 20.0, vertical: 20.0),
-                          child: TextField(
-                            maxLength: 25,
-                            controller: descTipoCategoriaController,
-                            decoration: InputDecoration(
-                              enabledBorder: OutlineInputBorder(
-                                  borderSide: new BorderSide(
-                                    color: kTextoColor,
-                                  ),
-                                  borderRadius: BorderRadius.circular(6.0)),
-                              labelText: 'Tipo de Categoria',
-                              hintText: 'Ingrese tipo categoria',
-                              labelStyle: textStyle,
-                              focusedBorder: OutlineInputBorder(
-                                  borderSide: new BorderSide(
-                                    color: kPrimaryColor,
-                                  ),
-                                  borderRadius: BorderRadius.circular(6.0)),
+                  child: Form(
+                    key: _formKeyTipoCategoria,
+                    child: Container(
+                      height: 250.0,
+                      child: Column(
+                        children: [
+                          Container(
+                            padding: EdgeInsets.all(18.0),
+                            height: 60.0,
+                            width: MediaQuery.of(context).size.width,
+                            decoration: BoxDecoration(
+                                color: kPrimaryColor,
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(15.0),
+                                    topRight: Radius.circular(15.0))),
+                            child: Text(
+                              "Registrar Tipo de Categoria",
+                              style: GoogleFonts.berkshireSwash(
+                                  color: kTextoLigthColor, fontSize: 18.0),
                             ),
                           ),
-                        ),
-                      ],
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 20.0, vertical: 20.0),
+                            child: TextFormField(
+                              validator: (valor) {
+                                if (valor.isEmpty) {
+                                  return 'Ingrese el tipo de categoria';
+                                }
+                                return null;
+                              },
+                              maxLength: 25,
+                              controller: descTipoCategoriaController,
+                              decoration: InputDecoration(
+                                labelText: 'Tipo de Categoria',
+                                // hintText: 'Ingrese tipo categoria',
+                                labelStyle: textStyle,
+                                enabledBorder: inputBorderTexto,
+                                focusedBorder: inputBorderTexto,
+                                errorBorder: inputBorderTexto,
+                                focusedErrorBorder: inputBorderTexto,
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.70,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                // Verifica si todo esta validadado
+                                if (_formKeyTipoCategoria.currentState
+                                    .validate()) {
+                                  // Aca se gurda la informacion
+                                }
+                              },
+                              child: Text("REGISTRAR"),
+                              style: ElevatedButton.styleFrom(
+                                  primary: kAcentColor,
+                                  padding: EdgeInsets.symmetric(vertical: 15.0),
+                                  elevation: 5.0,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(10.0))),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
